9b697035398c3818d432cd957bf1696001a2d188
Dima <57713733+Dima1409@users.noreply.github.com>
2023-04-19 00:09:13 +0300

    add router, fix 404

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import { store } from 'redux/store';
 import { Provider } from 'react-redux';
 import App from './App';
 import './index.css'
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <BrowserRouter basename='/technical-boost/'/>
        <App />
     </Provider>
   </React.StrictMode>
